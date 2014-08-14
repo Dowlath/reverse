65f42016fc4c10913b7608eab2b21055e483c92f
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-14 16:49:45 +0530

    reverse

--- reverse/main.m
+++ reverse/main.m
@@ -12,9 +12,17 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        int num,rev;
+        NSLog(@"enter the number");
+        scanf("%i",&num);
+        while (num !=0) {
+            rev=num%10;
+             num/=10;
+             NSLog(@"%i",rev);
+        }
         
         // insert code here...
-        NSLog(@"Hello, World!");
+       // NSLog(@"Hello, World!");
         
     }
     return 0;
